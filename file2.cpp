dda657af00a7363a525af2d7a378c66f1a2b9f3b
Olga <aos3@i.ua>
2015-10-07 09:42:38 +0300

    Third change

--- file2.cpp
+++ file2.cpp
@@ -1,8 +1,11 @@
 package lab1;
 
 public class Arr {
+	private static String s = "Time after time...";
+	//Коментарий
 	public static void main(String []args)
 	{
+		System.out.println("I've paid my dues \n" + s);
 		System.out.println("We are the champions!");
 	}
 
